8049083b94195ad148655702625292368e5d5feb
JMR97 <90755308+JMR97@users.noreply.github.com>
2025-05-08 17:09:37 +0600

    Add files via upload

---
+++ APICheck.py
@@ -0,0 +1,17 @@
+import pinecone
+
+# Set your Pinecone API key here
+PINECONE_API_KEY = "pcsk_79w6Vc_8MGpWAL1MCgKDaUmWYof4XpG93HjuGGib4PHPMYeneowvhbcr8scNgiRmCom6KW"
+
+# Initialize Pinecone client
+pinecone.init(api_key=PINECONE_API_KEY)
+
+try:
+    # Try listing the indexes to see if the API key works
+    indexes = pinecone.list_indexes()
+    print("Pinecone API key is valid!")
+    print("Existing indexes:", indexes)
+except pinecone.exceptions.AuthenticationError as e:
+    print("Invalid Pinecone API key. Error:", e)
+except Exception as e:
+    print("An error occurred:", e)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot AI Project',
+    version= '0.0.0',
+    author= 'Jotirmoy Roy',
+    author_email= 'roymoyjoti@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
